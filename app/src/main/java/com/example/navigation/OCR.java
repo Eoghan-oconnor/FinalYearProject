37b2703f75eab3900782531a193b1146dee64192
Eoghan O'Connor <oconnorjohn40@gmail.com>
2020-08-13 14:59:27 +0100

    Adding needed libraries

--- app/src/main/java/com/example/navigation/OCR.java
+++ app/src/main/java/com/example/navigation/OCR.java
@@ -1,8 +1,12 @@
 package com.example.navigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class OCR extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class OCR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_o_c_r);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.addImage){
+            showImageImportDialog();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
